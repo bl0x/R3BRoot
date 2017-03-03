5187d8f2dfb652689b597e2ca1da26c4127c413d
Bastian Löher <b.loeher@gsi.de>
2017-03-03 14:20:35 +0100

    Clean and refactor R3BPtofReader.

--- r3bsource/R3BPtofReader.cxx
+++ r3bsource/R3BPtofReader.cxx
@@ -11,13 +11,14 @@ extern "C" {
 }
 
 #define MAX_PTOF_PLANES 1
+#define N_TUBES_PER_PADDLE 2
 
 R3BPtofReader::R3BPtofReader(EXT_STR_h101_PTOF* data, UInt_t offset)
 	: R3BReader("R3BPtofReader")
 	, fData(data)
 	, fOffset(offset)
 	, fLogger(FairLogger::GetLogger())
-    , fArray(new TClonesArray("R3BPaddleTamexMappedData"))
+	, fArray(new TClonesArray("R3BPaddleTamexMappedData"))
 {
 }
 
@@ -38,202 +39,226 @@ Bool_t R3BPtofReader::Init(ext_data_struct_info *a_struct_info)
 		return kFALSE;
 	}
 
-    // Register output array in tree
-    FairRootManager::Instance()->Register("PtofMapped", "Land", fArray, kTRUE);
+	// Register output array in tree
+	FairRootManager::Instance()->Register("PtofMapped", "Land", fArray,
+	    kTRUE);
 
 
 	// initial clear (set number of hits to 0)
 	EXT_STR_h101_PTOF_onion* data = (EXT_STR_h101_PTOF_onion*)fData;
-	for (int d=0;d<MAX_PTOF_PLANES;d++)
-		for (int t=0;t<2;t++) // tube
+	for (int d = 0; d < MAX_PTOF_PLANES; d++) {
+		for (int t = 0; t < N_TUBES_PER_PADDLE; t++) {
+			data->PTOF_P[d].T[t].TFLM = 0;
+			data->PTOF_P[d].T[t].TFTM = 0;
+		}
+	}
+
+	return kTRUE;
+}
+
+Bool_t R3BPtofReader::ReadLeadingEdges(EXT_STR_h101_PTOF_onion *data,
+    int d, int t)
+{
+	// # of channels with data. not necessarly number
+	// of hits! (b/c multi hit)
+	uint32_t numChannels = data->PTOF_P[d].T[t].TFLM;
+
+	// loop over channels
+	// index in v for first item of current channel
+	uint32_t curChannelStart = 0;
+	for (int i = 0; i < numChannels; i++)
+	{
+		// bar number 1..65
+		uint32_t bar;
+		// index in v for first item of next channel
+		uint32_t nextChannelStart;
+
+		bar = data->PTOF_P[d].T[t].TFLMI[i];
+		nextChannelStart = data->PTOF_P[d].T[t].TFLME[i];
+
+		for (int j = curChannelStart; j < nextChannelStart; j++)
 		{
-			data->PTOF_P[d].T[t].TFLM=0;
-			data->PTOF_P[d].T[t].TFTM=0;
+			ReadLeadingEdgeChannel(data, d, t, bar, j);
 		}
 
+		curChannelStart = nextChannelStart;
+	}
+	return kTRUE;
+}
+
+Bool_t R3BPtofReader::ReadTrailingEdges(EXT_STR_h101_PTOF_onion *data,
+    int d, int t)
+{
+	// # of channels with data. not necessarly number
+	// of hits! (b/c multi hit)
+	uint32_t numChannels = data->PTOF_P[d].T[t].TFTM;
+
+	// loop over channels
+	// index in v for first item of current channel
+	uint32_t curChannelStart = 0;
+	for (int i = 0; i < numChannels; i++)
+	{
+		// or 1..65
+		uint32_t bar;
+		// index in v for first item of next channel
+		uint32_t nextChannelStart;
+		nextChannelStart = data->PTOF_P[d].T[t].TFTME[i];
+		bar = data->PTOF_P[d].T[t].TFTMI[i];
+
+		for (int j = curChannelStart; j < nextChannelStart; j++)
+		{
+			ReadTrailingEdgeChannel(data, d, t, bar, j);
+		}
+
+		curChannelStart=nextChannelStart;
+	}
+	return kTRUE;
+}
+
+#define MAX_TIME_DIFF_PADDLE_PMT 20 /* 20 * 5 ns = 100 ns */
+Bool_t R3BPtofReader::ReadLeadingEdgeChannel(EXT_STR_h101_PTOF_onion *data,
+    int d, int t, uint32_t bar, int ch)
+{
+	R3BPaddleTamexMappedData* mapped = NULL;
+
+	/*
+	 * see if we can find a mappedData with
+	 * matching PM1 leading coarse time
+	 */
+	if (t == 1)
+	{
+		int n = fArray->GetEntriesFast();
+		int coarse = data->PTOF_P[d].T[t].TCLv[ch];
+
+		for (int k = 0; k < n; k++)
+		{
+			R3BPaddleTamexMappedData* hit =
+			    (R3BPaddleTamexMappedData*)fArray->At(k);
+
+			/*
+			 * if leading time1 within 100ns window
+			 * and time2 not yet set
+			 */
+			if ((hit->fCoarseTime2LE == 0)
+			    && (abs(hit->fCoarseTime1LE - coarse)
+				    <= MAX_TIME_DIFF_PADDLE_PMT))
+			{
+				mapped = hit;
+				break;
+			}
+		}
+	}
+
+	/*
+	 * Add a new hit to the array, if t == 0 or
+	 * No corresponding hit in PM1 found
+	 */
+	if (!mapped) mapped = new ((*fArray)[fArray->GetEntriesFast()])
+		R3BPaddleTamexMappedData(d + 1, bar); // plane, bar
+
+	/* Fill leading edge time members */
+	if (t == 0)
+	{
+		// PM1
+		mapped->fCoarseTime1LE = data->PTOF_P[d].T[t].TCLv[ch];
+		mapped->fFineTime1LE   = data->PTOF_P[d].T[t].TFLv[ch];
+	} else {
+		// PM2
+		mapped->fCoarseTime2LE = data->PTOF_P[d].T[t].TCLv[ch];
+		mapped->fFineTime2LE   = data->PTOF_P[d].T[t].TFLv[ch];
+	}
+	return kTRUE;
+}
+
+#define MAX_TIME_OVER_THRESHOLD 1000 /* 1000 * 5 ns = 5 us */
+Bool_t R3BPtofReader::ReadTrailingEdgeChannel(EXT_STR_h101_PTOF_onion *data,
+    int d, int t, uint32_t bar, int ch)
+{
+	R3BPaddleTamexMappedData* mapped = NULL;
+
+	// see if we can find a mappedData with matching PM1 data
+	int n = fArray->GetEntriesFast();
+	int coarse = data->PTOF_P[d].T[t].TCTv[ch];
+	// matching a trailing time is only useful if the corresponding
+	// leading time is present. Meaning: no need to check trailing1
+	// to leading2 and vice-versa. This reduces the combinations we have to 
+	// check to:
+	// match trailing2 to leading2
+	// match trailing1 to leading1
+
+	// do the if outside to most inner loop for performance reasons
+	if (t == 0) {
+		// PM1
+		for (int k = 0; k < n; k++)
+		{
+			R3BPaddleTamexMappedData* hit =
+			    (R3BPaddleTamexMappedData*)fArray->At(k);
+			int tot = coarse - hit->fCoarseTime1LE;
+			if ((tot <= MAX_TIME_OVER_THRESHOLD) && (tot >= 0)
+			    && (hit->fCoarseTime1TE == 0) /* no trailing */
+			    && (hit->fCoarseTime1LE != 0)) /* has leading */
+			{
+				mapped = hit;
+				break;
+			}
+		}
+	} else {
+		// PM2
+		for (int k = 0; k < n; k++)
+		{
+			R3BPaddleTamexMappedData* hit =
+			    (R3BPaddleTamexMappedData*)fArray->At(k);
+			int tot = coarse - hit->fCoarseTime2LE;
+			if ((tot <= MAX_TIME_OVER_THRESHOLD) && (tot >= 0)
+			    && (hit->fCoarseTime2TE == 0) /* no trailing */
+			    && (hit->fCoarseTime2LE != 0)) /* has leading */
+			{
+				mapped = hit;
+				break;
+			}
+		}
+	}
+
+	if (!mapped) mapped = new ((*fArray)[fArray->GetEntriesFast()])
+		R3BPaddleTamexMappedData(d + 1, bar); // plane, bar
+
+	if (t == 0)
+	{
+		// PM1
+		// coarse time leading edge
+		mapped->fCoarseTime1TE= data->PTOF_P[d].T[t].TCTv[ch];
+		// fine time leading edge
+		mapped->fFineTime1TE  = data->PTOF_P[d].T[t].TFTv[ch];
+	} else {
+		// PM2
+		// coarse time leading edge
+		mapped->fCoarseTime2TE= data->PTOF_P[d].T[t].TCTv[ch];
+		// fine time leading edge
+		mapped->fFineTime2TE  = data->PTOF_P[d].T[t].TFTv[ch];
+	}
 	return kTRUE;
 }
 
 Bool_t R3BPtofReader::Read()
 {
 	// Convert plain raw data to multi-dimensional array
-    EXT_STR_h101_PTOF_onion* data = (EXT_STR_h101_PTOF_onion*)fData;
-
-#if 0
-
-  struct {
-    struct {
-      uint32_t TFLM;
-      uint32_t TFLMI[4 /* TFLM */];
-      uint32_t TFLME[4 /* TFLM */];
-      uint32_t TFL;
-      uint32_t TFLv[40 /* TFL */];
-      uint32_t TFTM;
-      uint32_t TFTMI[4 /* TFTM */];
-      uint32_t TFTME[4 /* TFTM */];
-      uint32_t TFT;
-      uint32_t TFTv[40 /* TFT */];
-      uint32_t TCLM;
-      uint32_t TCLMI[4 /* TCLM */];
-      uint32_t TCLME[4 /* TCLM */];
-      uint32_t TCL;
-      uint32_t TCLv[40 /* TCL */];
-      uint32_t TCTM;
-      uint32_t TCTMI[4 /* TCTM */];
-      uint32_t TCTME[4 /* TCTM */];
-      uint32_t TCT;
-      uint32_t TCTv[40 /* TCT */];
-    } T[2];
-  } PTOF_P[1];
+	EXT_STR_h101_PTOF_onion* data = (EXT_STR_h101_PTOF_onion*)fData;
 
 
-#endif
-
-
-
-	
-	for (int d=0;d<MAX_PTOF_PLANES;d++)  // loop over all planes
-		for (int t=0;t<2;t++)            // loop over tube 1 and 2
+	for (int d = 0; d < MAX_PTOF_PLANES; d++)
+	{
+		for (int t = 0; t < N_TUBES_PER_PADDLE; t++)
 		{
-			// ******** leading edges ********
-			
-			// # of channels with data. not necessarly number of hits! (b/c multi hit)
-			uint32_t numChannels = data->PTOF_P[d].T[t].TFLM; 
-				
-			// loop over channels
-			uint32_t curChannelStart=0;     // index in v for first item of current channel
-			for (int i=0;i<numChannels;i++) 
-			{
-				uint32_t channel=data->PTOF_P[d].T[t].TFLMI[i]; // or 1..65
-				uint32_t nextChannelStart=data->PTOF_P[d].T[t].TFLME[i];  // index in v for first item of next channel
-				
-				for (int j=curChannelStart;j<nextChannelStart;j++) 
-				{
-					
-					R3BPaddleTamexMappedData* mapped=NULL;
-
-					// see if we can find a mappedData with matching PM1 leading coarse time
-					if (t==1)
-					{ 
-						int n=fArray->GetEntriesFast();
-						int coarse=data->PTOF_P[d].T[t].TCLv[j];
-						for (int k=0;k<n;k++)
-						{
-							R3BPaddleTamexMappedData* hit = (R3BPaddleTamexMappedData*)fArray->At(k);
-							// if leading time1 within 100ns window and time2 not yet set
-							if ((hit->fCoarseTime2LE==0) && (abs(hit->fCoarseTime1LE - coarse)<=20))
-							{
-								mapped=hit;
-								break;
-							}
-						}
-					}
-					
-					if (!mapped) mapped=new ((*fArray)[fArray->GetEntriesFast()])
-						R3BPaddleTamexMappedData(d+1,    // 1..n   (plane)
-							channel);				     // 1..n   (bar)
-
-					if (t==0) // PM1
-					{
-						mapped->fCoarseTime1LE= data->PTOF_P[d].T[t].TCLv[j];  // coarse time leading edge
-						mapped->fFineTime1LE  = data->PTOF_P[d].T[t].TFLv[j];  // fine time leading edge
-					}
-					else // PM2
-					{
-						mapped->fCoarseTime2LE= data->PTOF_P[d].T[t].TCLv[j];  // coarse time leading edge
-						mapped->fFineTime2LE  = data->PTOF_P[d].T[t].TFLv[j];  // fine time leading edge
-					}
-				}
-				
-				curChannelStart=nextChannelStart;
-			} // for leading edges
-			
-			
-
-
-			// ******** trailing edges ********
-			
-			// # of channels with data. not necessarly number of hits! (b/c multi hit)
-			numChannels = data->PTOF_P[d].T[t].TFTM; 
-				
-			// loop over channels
-			curChannelStart=0;     // index in v for first item of current channel
-			for (int i=0;i<numChannels;i++) 
-			{
-				uint32_t channel=data->PTOF_P[d].T[t].TFTMI[i]; // or 1..65
-				uint32_t nextChannelStart=data->PTOF_P[d].T[t].TFTME[i];  // index in v for first item of next channel
-				
-				for (int j=curChannelStart;j<nextChannelStart;j++) {
-					
-					R3BPaddleTamexMappedData* mapped=NULL;
-
-					// see if we can find a mappedData with matching PM1 data
-					int n=fArray->GetEntriesFast();
-					int coarse=data->PTOF_P[d].T[t].TCTv[j];
-					// matching a trailing time is only useful if the corresponding
-					// leading time is present. Meaning: no need to check trailing1
-					// to leading2 and vice-versa. This reduces the combinations we have to 
-					// check to:
-					// match trailing2 to leading2
-					// match trailing1 to leading1
-					
-					if (t==0) // do the if outside to most inner loop for performance reasons
-						 // PM1
-						for (int k=0;k<n;k++)
-						{
-							R3BPaddleTamexMappedData* hit = (R3BPaddleTamexMappedData*)fArray->At(k);
-							int tot=coarse - hit->fCoarseTime1LE;
-							if ((tot<=1000) && (tot>=0) &&   // time-over-treshold reasonable
-								(hit->fCoarseTime1TE==0) &&  // trailing not yet set
-								(hit->fCoarseTime1LE!=0))    // leading IS set (else tot is crap)
-							{
-								mapped=hit;
-								break;
-							}
-						}
-					else // PM2
-						for (int k=0;k<n;k++)
-						{
-							R3BPaddleTamexMappedData* hit = (R3BPaddleTamexMappedData*)fArray->At(k);
-							// if leading time within 5000ns window and trailing2 not yet set
-							int tot=coarse - hit->fCoarseTime2LE;
-							if ((tot<=1000) && (tot>=0) &&   // time-over-treshold reasonable
-								(hit->fCoarseTime2TE==0) &&  // trailing not yet set
-								(hit->fCoarseTime2LE!=0))    // leading IS set
-							{
-								mapped=hit;
-								break;
-							}
-						}
-					
-					if (!mapped) mapped=new ((*fArray)[fArray->GetEntriesFast()])
-						R3BPaddleTamexMappedData(d+1,    // 1..n   (plane)
-							channel);				     // 1..n   (bar)
-
-					if (t==0) // PM1
-					{
-						mapped->fCoarseTime1TE= data->PTOF_P[d].T[t].TCTv[j];  // coarse time leading edge
-						mapped->fFineTime1TE  = data->PTOF_P[d].T[t].TFTv[j];  // fine time leading edge
-					}
-					else      // PM2
-					{
-						mapped->fCoarseTime2TE= data->PTOF_P[d].T[t].TCTv[j];  // coarse time leading edge
-						mapped->fFineTime2TE  = data->PTOF_P[d].T[t].TFTv[j];  // fine time leading edge
-					}
-				}
-				
-				curChannelStart=nextChannelStart;
-			} // for trailing edges
-
-		} // for tubes
-
-
+			ReadLeadingEdges(data, d, t);
+			ReadTrailingEdges(data, d, t);
+		}
+	}
 }
 
 void R3BPtofReader::Reset()
 {
-    // Reset the output array
-    fArray->Clear();
+	// Reset the output array
+	fArray->Clear();
 }
 
 ClassImp(R3BPtofReader)
--- r3bsource/R3BPtofReader.h
+++ r3bsource/R3BPtofReader.h
@@ -5,6 +5,7 @@
 
 struct EXT_STR_h101_PTOF_t;
 typedef struct EXT_STR_h101_PTOF_t EXT_STR_h101_PTOF;
+typedef struct EXT_STR_h101_PTOF_onion_t EXT_STR_h101_PTOF_onion;
 class FairLogger;
 
 class R3BPtofReader : public R3BReader
@@ -27,10 +28,46 @@ class R3BPtofReader : public R3BReader
 		/* the structs of type R3BPtofxMappedItem */
 		TClonesArray* fArray; /**< Output array. */
 
+		Bool_t ReadLeadingEdges(EXT_STR_h101_PTOF_onion *, int, int);
+		Bool_t ReadTrailingEdges(EXT_STR_h101_PTOF_onion *, int, int);
+		Bool_t ReadLeadingEdgeChannel(EXT_STR_h101_PTOF_onion *, int,
+		    int, uint32_t, int);
+		Bool_t ReadTrailingEdgeChannel(EXT_STR_h101_PTOF_onion *, int,
+		    int, uint32_t, int);
 
 	public:
 		ClassDef(R3BPtofReader, 0);
 };
+
+#if 0
+/* Structure layout from ext_h101_ptof.h */
+struct {
+	struct {
+		uint32_t TFLM;
+		uint32_t TFLMI[4 /* TFLM */];
+		uint32_t TFLME[4 /* TFLM */];
+		uint32_t TFL;
+		uint32_t TFLv[40 /* TFL */];
+		uint32_t TFTM;
+		uint32_t TFTMI[4 /* TFTM */];
+		uint32_t TFTME[4 /* TFTM */];
+		uint32_t TFT;
+		uint32_t TFTv[40 /* TFT */];
+		uint32_t TCLM;
+		uint32_t TCLMI[4 /* TCLM */];
+		uint32_t TCLME[4 /* TCLM */];
+		uint32_t TCL;
+		uint32_t TCLv[40 /* TCL */];
+		uint32_t TCTM;
+		uint32_t TCTMI[4 /* TCTM */];
+		uint32_t TCTME[4 /* TCTM */];
+		uint32_t TCT;
+		uint32_t TCTv[40 /* TCT */];
+	} T[2];
+} PTOF_P[1];
+
+
+#endif
 
 #endif
 
